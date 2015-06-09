0fdb7a6d74fdbd4f45edc2a8cd5b20301a9d1d65
norman chang <nsc.chang@gmail.com>
2015-06-09 12:11:40 +0800

    Add flow3.rb answer

---
+++ flow3.rb
@@ -0,0 +1,11 @@
+def number (num)
+  if num<=50
+    puts "this number is equal and below 50"
+  elsif num<=100
+    puts "this number is equal and below 100"
+  else
+    puts "this number is above 100"
+  end
+end
+
+number(20)
