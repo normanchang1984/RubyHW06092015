19ed104ad904573a3fe4c1b4511ca2e8adf4269e
norman chang <nsc.chang@gmail.com>
2015-06-09 17:51:15 +0800

    Add classobject2.rb answer

---
+++ classobject2.rb
@@ -0,0 +1,34 @@
+class MyCar
+  attr_accessor :color
+  attr_reader :year
+  def initialize(year,color,model)
+    @year=year
+    @color=color
+    @model=model
+    @speed=0
+  end
+
+  def meter_speed(speed)
+    @speed+=speed
+    puts "Now the current speed is #{@speed}"
+  end
+
+  def need_brake(brake)
+    @speed-=brake
+    puts "Now the current speed is #{@speed}"
+  end
+
+  def shutdown
+    @speed=0
+    puts "Car is now turned-off"
+  end
+
+end
+
+mybmw=MyCar.new(2013,"grey","328i")
+mybmw.meter_speed(30)
+mybmw.need_brake(20)
+mybmw.shutdown()
+mybmw.color="blue"
+puts mybmw.color
+puts mybmw.year
