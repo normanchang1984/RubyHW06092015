1c65ffa4a75ecfa4027cb0bd39ee0b8392adf9d8
norman chang <nsc.chang@gmail.com>
2015-06-09 11:44:22 +0800

    Add basic3 answer

---
+++ basic3.rb
@@ -0,0 +1,6 @@
+movielist={:movie1=>1975, :movie2=>2004, :movie3=>2013, :movie4=>2001, :movie5=>1981}
+puts movielist[:movie1]
+puts movielist[:movie2]
+puts movielist[:movie3]
+puts movielist[:movie4]
+puts movielist[:movie5]
